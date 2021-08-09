4c5199c2fee53610e7b518922b42c0c17764e799
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-08-09 17:11:41 +0700

    [Store] Add some authentication - When response status 401, the app would force logout and redirect to login - When token is not available at Vuex, the app would force logout and redirect to login too

--- src/store/constants/mutations.type.js
+++ src/store/constants/mutations.type.js
@@ -2,6 +2,7 @@ export const GET_AUTHENTICATED = `getAuthenticated`;
 export const SET_AUTHENTICATED = "setAuthenticated";
 export const SET_LOADING = "setLoading";
 export const SET_AUTH = "setAuth";
+export const PURGE_AUTH = "purgeAuth";
 export const CHECK_AUTH = "checkAuth";
 
 export const RESET_IDENTIY = "resetIdentiy";
